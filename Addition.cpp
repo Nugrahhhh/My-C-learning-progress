1c3ae79d8e65dda4d81045f7a632412c2720512e
Nugrah Reswara <nugrahreswara@gmail.com>
2024-10-07 08:58:39 +0800

    Update Addition.cpp

--- Addition.cpp
+++ Addition.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
-#include <string.h>
 
 using namespace std;
 
-int a;
-int b;
-
 int main()
 {
-    cout << "Input A: "; cin >> a ;
+    
+   int a;
+   int b; 
+
+    cout << "Input A: "; cin >> a;
 
     cout << "Input B: "; cin >> b;
 
     cout << "The answer is: " << a + b << endl;
 
     return 0;
+    
 }
