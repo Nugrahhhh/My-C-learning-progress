f1d5e5d34e5ada825d0cb20f71ed840bf20f1b1c
Nugrah Reswara <nugrahreswara@gmail.com>
2024-10-07 09:16:21 +0800

    Math operation

---
+++ Math operation/Multiplication.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a;
+    int b;
+
+    cout << "Input A: "; cin >> a;
+    cout << "Input B: "; cin >> b;
+
+    cout << "The answer is: " << a * b << endl;
+
+    return 0; 
+
+}
