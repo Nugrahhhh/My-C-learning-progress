8f0f63844f068c5564c0e6cac3129c5bcb645814
Nugrah Reswara <nugrahreswara@gmail.com>
2024-10-07 08:50:39 +0800

    Update Addition.cpp

--- Addition.cpp
+++ Addition.cpp
@@ -8,11 +8,9 @@ int b;
 
 int main()
 {
-    cout << "Input A: " << endl;
-    cin >> a;
+    cout << "Input A: "; cin >> a ;
 
-    cout << "Input B: " << endl;
-    cin >> b;
+    cout << "Input B: "; cin >> b;
 
     cout << "The answer is: " << a + b << endl;
 
